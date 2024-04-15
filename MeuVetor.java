522455835fc5102aa4f3989b7d48f9224be1705e
Sophia Sissi <sophiacurcioguedes@gmail.com>
2024-04-15 10:40:10 -0300

    Definindo Métodos

--- MeuVetor.java
+++ MeuVetor.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class MeuVetor {
     private double[] v;
     private int ultimaPos;
@@ -8,14 +6,16 @@ public class MeuVetor {
         v = new double[capacidade];
         ultimaPos = -1;
     }
-    //métodos de acesso
+
+    //Métodos de Acesso 
     public int getUltimaPos() {
         return ultimaPos;
     }
     public double[] getV() {
         return v;
     }
-    //métodos modificadores
+
+    //Métodos Modificadores
     public void setUltimaPosicao(int pos) {
         if (pos >= 0 && pos < v.length) {
             ultimaPos = pos;
@@ -28,79 +28,21 @@ public class MeuVetor {
     public boolean estaCheio () {
         return ultimaPos == v.length-1;
     }
-
     public boolean estaVazio () {
         return ultimaPos == -1;
     }
-
     public void resetVetor() {
         ultimaPos = -1;
     }
 
-    public void adiciona(int elemento) {
-        if (estaCheio()) {
-            redimensiona(v.length*2);
-        }
-        v[++ultimaPos] = elemento;
-    }
-
+    //Método para adicionar elementos em um vetor 
     public void adiciona (double elemento) {
         if (estaCheio())
             redimensiona(v.length*2);
         v[++ultimaPos] = elemento;
     }
 
-    public void adiciona (double elemento, int posicao) {
-        if (estaCheio())
-            redimensiona(v.length*2);
-        if (posicao > ultimaPos)
-            v[++ultimaPos] = elemento;
-        else {
-            //deslocar elementos
-            int i;
-            for (i=ultimaPos+1; i>posicao; i--)
-                v[i] = v[i-1];
-            v[i] = elemento;
-            ultimaPos++;
-        }
-    }
-    
-    public double remove() {
-        if (estaVazio()) return 0;
-        double aux = v[ultimaPos--];
-        if (v.length >= 10 && ultimaPos <= v.length / 4) {
-            redimensiona(v.length/2);
-        }
-        return aux;
-    }
-    public double remove(int posicao) {
-        if (estaVazio() || posicao > ultimaPos) return 0;
-        double aux = v[posicao];
-        for (int i = posicao; i<ultimaPos; i++)
-            v[i] = v[i+1];
-        ultimaPos--;
-        if (v.length >= 10 && ultimaPos <= v.length / 4) {
-            redimensiona(v.length/2);
-        }
-        return aux;
-    }
-    public int remove (double elemento) {
-        if (estaVazio()) return 0;
-        int cont = 0;
-        for (int i=ultimaPos; i>=0; i--) {
-            if (v[i] == elemento) {
-                //arrasta
-                for (int j=i; j<ultimaPos; j++)
-                    v[j] = v[j+1];
-                ultimaPos--;
-                cont++;
-            }
-        }
-        if (v.length >= 10 && ultimaPos <= v.length / 4) {
-            redimensiona(v.length/2);
-        }
-        return cont;
-    }
+    //Método para redimensionar o vetor
     private void redimensiona(int novaCapacidade) {
         double[] temp = new double[novaCapacidade];
         for (int i = 0; i <= ultimaPos; i++) {
@@ -108,6 +50,44 @@ public class MeuVetor {
         }
         v = temp;
     }
+
+    //Método Bubble Sort
+    public int bubbleSort () {
+        int cont = 0;
+        for (int i=1; i<v.length; i++) {
+            for (int j=0; j<v.length-i; j++) {
+                cont++;
+                if (v[j] > v[j+1]) {
+                    double aux = v[j];
+                    v[j] = v[j+1];
+                    v[j+1] = aux;
+                }
+            }
+        }
+        return cont;
+    }
+
+    //Método Insertion Sort
+    public void insertionSort (int n, int v[]) {
+       for (int j = 1; j < n; ++j) {
+          int x = v[j];
+          int i;
+          for (i = j-1; i >= 0 && v[i] > x; --i) 
+             v[i+1] = v[i];
+          v[i+1] = x;
+       }
+    }
+
+    //Método Selection Sort
+    public void selectionSort (int n, int v[]) {
+       for (int i = 0; i < n-1; ++i) {
+          int min = i;
+          for (int j = i+1; j < n; ++j)
+             if (v[j] < v[min])  min = j;
+          int x = v[i]; v[i] = v[min]; v[min] = x;
+       }
+    }
+
 
     @Override
     public String toString() {
@@ -124,61 +104,4 @@ public class MeuVetor {
         return s;
     }
 
-    public void preencheVetor () {
-        Random r = new Random();
-        for (int i=0; i<v.length; i++) {
-            adiciona(r.nextInt(v.length*10)+1);
-        }
-    }
-    public void preencheVetor (int limite) {
-        Random r = new Random();
-        for (int i=0; i<v.length; i++) {
-            adiciona(r.nextInt(limite));
-        }
-    }
-    public int bubbleSort () {
-        int cont = 0;
-        for (int i=1; i<v.length; i++) {
-            for (int j=0; j<v.length-i; j++) {
-                cont++;
-                if (v[j] > v[j+1]) {
-                    double aux = v[j];
-                    v[j] = v[j+1];
-                    v[j+1] = aux;
-                }
-            }
-        }
-        return cont;
-    }
-    public Retorno buscaSimples (double x) {
-        Retorno r = new Retorno();
-        for (int i=0; i<=ultimaPos; i++) {
-            r.incrementaContador();
-            if (x == v[i]) {
-                r.setAchou(true);
-                return r;
-            }
-        }
-        return r;
-    }
-    public Retorno buscaBinaria (double x) {
-        int inicio = 0, fim = ultimaPos;
-        int meio;
-        Retorno r = new Retorno();
-        while (inicio <= fim) {
-            meio = (inicio + fim) / 2;
-            r.incrementaContador();
-            if (x == v[meio]) {
-                r.setAchou(true);
-                return r;
-            }
-            if (x > v[meio]) {
-                inicio = meio + 1;
-            }
-            else {
-                fim = meio - 1;
-            }
-        }
-        return r;
-    }
 }
