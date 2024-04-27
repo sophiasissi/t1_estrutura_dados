c174e831855b2d7575b508b6fc68faa994849ae0
Sophia Sissi <sophiacurcioguedes@gmail.com>
2024-04-27 17:56:06 -0300

    Finalização dos testes

--- graficos.py
+++ graficos.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('dados.csv')
 
 plt.figure(figsize=(10, 6))
+y = data['Tamanho do Vetor']
 
 # Gráfico - Bubble Sort
-plt.plot(data['Tempo BubbleSort (ms)'], data['Tamanho do Vetor'], label='Bubble Sort')
+plt.plot(data['Tempo BubbleSort (ms)'], y, label='Bubble Sort')
 
-# Gráfico - Insertion Sort
-plt.plot(data['Tempo InsertionSort (ms)'], data['Tamanho do Vetor'], label='Insertion Sort')
+# # Gráfico - Insertion Sort
+# plt.plot(data['Tempo InsertionSort (ms)'], y, label='Insertion Sort')
 
-# Gráfico - Selection Sort
-plt.plot(data['Tempo SelectionSort (ms)'], data['Tamanho do Vetor'], label='Selection Sort')
+# # Gráfico - Selection Sort
+# plt.plot(data['Tempo SelectionSort (ms)'], y, label='Selection Sort')
 
-plt.xlim(0, 1500000)
+# plt.xlim(0, 1500000)
 plt.xlabel('Tamanho do Vetor')
 plt.ylabel('Tempo (ms)')
 plt.title('Tempos de Execução em função do Tamanho')
